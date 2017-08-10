97e8306b14d5f4a2cfbe5ea4d2f3adb51173b83c
Guinslym <guinslym@gmail.com>
2017-08-10 17:44:55 -0400

    finish

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,32 @@
 import  React from 'react';
-import {Testimonial} from '../components/Testimonial';
+import {LibrarySelect} from '../components/LibrarySelect';
+import Branches from '../data/branches.json';
 
 class App extends React.Component {
   constructor(props){
     super(props);
-    this.state = {'data': ""};
+    this.state = {
+      BranchInfo: Branches.BranchesInfo.BranchInfo,
+      libraryName: 'Orleans'
+      };
+      this.changeLibraryName = this.changeLibraryName.bind(this);
+  }
+
+  changeLibraryName(newName){
+    this.setState({
+      libraryName: newName
+    })
+  }
+
   render() {
     return (
       <div className="container">
-        <h1 className="text-center">ReactJs SelectField Tutorial</h1>
 
-
-        <div className="row">
-
-  			</div>
+          <LibrarySelect
+            libraries={this.state.BranchInfo}
+            title={this.state.libraryName}
+            onChange={this.changeLibraryName}
+          />
 
       </div>
     );
---
+++ src/components/LibrarySelect.js
@@ -0,0 +1,37 @@
+import React, {Component} from 'react';
+
+export class LibrarySelect extends Component{
+  constructor(props){
+    super(props);
+    this.handleSelect = this.handleSelect.bind(this);
+  }
+
+
+  handleSelect(e){
+    const name = e.target.value;
+    console.log("Name Selected: " + name);
+    this.props.onChange(name);
+  }
+
+
+  render(){
+    return(
+      <div>
+          <h1 className="text-center">{this.props.title}</h1>
+          <div className="row">
+            <div className="form-group">
+              <label htmlFor="sel1">Select list:</label>
+              <select className="form-control" id="sel1" onChange={this.handleSelect}>
+                  {this.props.libraries.map((library, i) =>
+                    <option key={i} value={library.name}>
+                      {library.Name}
+                    </option>
+                    )}
+               </select>
+            </div>
+          </div>
+      </div>
+
+    );
+  }
+}
